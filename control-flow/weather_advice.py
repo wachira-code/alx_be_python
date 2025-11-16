51c8ba44c13f7a5e99903bd1c843455a9c05d48f
Mofat Wachira <mofatwachira01@gmail.com>
2025-11-16 20:49:56 +0300

    Made corrections to the code

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -7,4 +7,4 @@ elif weather == "rainy":
 elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Sorry, I don't have recommendations for this weather.")
+    print("Sorry, I don't have a recommendation for that weather.")
