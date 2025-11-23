728764eb6104a5f445f749915ed56b67f878e16d
Mofat Wachira <mofatwachira01@gmail.com>
2025-11-23 21:10:38 +0300

    Second commit for arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation:str):
+def perform_operation(num1: float,num2: float,operation: str):
     operation = operation.lower()
 
     if operation == "add":
