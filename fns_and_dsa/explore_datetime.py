349cd76c87581c241317d582c003fdb6acf924fd
Mofat Wachira <mofatwachira01@gmail.com>
2025-11-24 00:47:19 +0300

    date and time calculation

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Date and time: {formatted_datetime}")
+
+    return current_date
+
+if __name__ == "__main__":
+    display_current_datetime()
+
+from datetime import datetime, timedelta
+def display_current_datetime():
+    
+    current_date = datetime.now()
+    formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Date and time: {formatted_datetime}")
+    return current_date
+
+def calculate_future_date():
+    days = int(input("Enter the number of days to add: "))
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days)
+
+    formatted_future_date = future_date.strftime("%Y-%M-%d")
+    print(f"Future Date: {formatted_future_date}")
+    return future_date
+
+if __name__ == "__main__":
+    print("Calculate a future date ===")
+    calculate_future_date()
+        
+    
