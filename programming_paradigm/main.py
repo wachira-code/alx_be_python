994ccd98587554d673b813c76f0b11e506d348f3
Mofat Wachira <mofatwachira01@gmail.com>
2025-11-30 23:26:30 +0300

    Library Management main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -14,3 +14,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+from library_management import Book, Library
+library = Library()
+
+library.add_book(Book("Atomic Habits", "James Clear"))
+library.add_bbok(Book("Python Crash Coarse", "Eric Matthes"))
+library.add_book(Book("The alchemist", "Paulo Coelho"))
+
+print(library.check_out_book("Atomic Habits"))
+print("Available books:", library.list_available_books())
+print(library.return_book("Atomic Habits"))
+print("Available books:", library.list_available_books())
