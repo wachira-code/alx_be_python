27c80082a84bc099e912c8caaf96235bdcde94fd
Mofat Wachira <mofatwachira01@gmail.com>
2025-11-30 10:23:35 +0300

    Main file for bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,6 +15,6 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
-         
+        print(f"Current Balance: ${self.account_balance:.2f}")
+
         
