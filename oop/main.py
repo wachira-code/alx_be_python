c437e97d28fe1b65a109e63aab38fdb9ca4edf0f
Mofat Wachira <mofatwachira01@gmail.com>
2025-12-07 21:17:43 +0300

    Class and Static methods main.py

--- oop/main.py
+++ oop/main.py
@@ -45,3 +45,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+from class_static_methods_demo import Calculator
+def main():
+    sum_result = Calculator.add(10,5)
+    print(f"The sum is: {sum_result}")
+
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
+
+if __name__ == "__main__":
+    main()
+
+
